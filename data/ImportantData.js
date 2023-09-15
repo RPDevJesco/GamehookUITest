105dd35b15d6dface66f86a68e69a6c1b2f53d09
Jesse Glover <jglover88@hotmail.com>
2023-09-14 19:37:58 -0500

    test

---
+++ data/ImportantData.js
@@ -0,0 +1,180 @@
+const TmHmList = [{
+    "TM01": "Mega Punch",
+    "TM02": "Razor Wind",
+    "TM03": "Swords Dance",
+    "TM04": "Whirlwind",
+    "TM05": "Mega Kick",
+    "TM06": "Toxic",
+    "TM07": "Horn Drill",
+    "TM08": "Body Slam",
+    "TM09": "Take Down",
+    "TM10": "Double-edge",
+    "TM11": "Bubblebeam",
+    "TM12": "Water Gun",
+    "TM13": "Ice Beam",
+    "TM14": "Blizzard",
+    "TM15": "Hyper Beam",
+    "TM16": "Pay Day",
+    "TM17": "Submission",
+    "TM18": "Counter",
+    "TM19": "Seismic Toss",
+    "TM20": "Rage",
+    "TM21": "Mega Drain",
+    "TM22": "Solarbeam",
+    "TM23": "Dragon Rage",
+    "TM24": "Thunderbolt",
+    "TM25": "Thunder",
+    "TM26": "Earthquake",
+    "TM27": "Fissure",
+    "TM28": "Dig",
+    "TM29": "Psychic",
+    "TM30": "Teleport",
+    "TM31": "Mimic",
+    "TM32": "Double Team",
+    "TM33": "Reflect",
+    "TM34": "Bide",
+    "TM35": "Metronome",
+    "TM36": "Selfdestruct",
+    "TM37": "Egg Bomb",
+    "TM38": "Fire Blast",
+    "TM39": "Swift",
+    "TM40": "Skull Bash",
+    "TM41": "Softboiled",
+    "TM42": "Dream Eater",
+    "TM43": "Sky Attack",
+    "TM44": "Rest",
+    "TM45": "Thunder Wave",
+    "TM46": "Psywave",
+    "TM47": "Explosion",
+    "TM48": "Rock Slide",
+    "TM49": "Tri Attack",
+    "TM50": "Substitute",
+    "HM01": "Cut",
+    "HM02": "Fly",
+    "HM03": "Surf",
+    "HM04": "Strength",
+    "HM05": "Flash"
+}];
+
+const keyItems = [{
+    "TOWN MAP" : {
+        "itemLocation": "Pallet Town - Blue's House",
+        "imgLink": "./Images/keyItems/townmap.webp",
+        "hasCheckedLocation": false
+    },
+    "BICYCLE" : {
+        "itemLocation": "Cerulean City - Bike Shop",
+        "imgLink": "./Images/keyItems/bicycle.webp",
+        "hasCheckedLocation": false
+    },
+    "BIKE VOUCHER" : {
+        "itemLocation": "Vermilion City - Poke Club",
+        "imgLink": "./Images/keyItems/bikevoucher.webp",
+        "hasCheckedLocation": false
+    },
+    "CARD KEY" : {
+        "itemLocation": "Silph Co - 5F",
+        "imgLink": "./Images/keyItems/cardkey.webp",
+        "hasCheckedLocation": false
+    },
+    "COIN CASE" : {
+        "itemLocation": "Celadon City - Diner",
+        "imgLink": "./Images/keyItems/coincase.webp",
+        "hasCheckedLocation": false
+    },
+    "DOME FOSSIL" : {
+        "itemLocation": "Mt Moon - 3",
+        "imgLink": "./Images/keyItems/pgl/domefossil.webp",
+        "hasCheckedLocation": false
+    },
+    "GOLD TEETH" : {
+        "itemLocation": "Safari Zone (West)",
+        "imgLink": "./Images/keyItems/goldteeth.webp",
+        "hasCheckedLocation": false
+    },
+    "GOOD ROD" : {
+        "itemLocation": "Fuchsia City - House 3",
+        "imgLink": "./Images/keyItems/goodrod.webp",
+        "hasCheckedLocation": false
+    },
+    "HELIX FOSSIL" : {
+        "itemLocation": "Mt Moon - 3",
+        "imgLink": "./Images/keyItems/helixfossil.webp",
+        "hasCheckedLocation": false
+    },
+    "ITEMFINDER" : {
+        "itemLocation": "Route 11 - Gate 2F",
+        "imgLink": "./Images/keyItems/itemfinder.webp",
+        "hasCheckedLocation": false
+    },
+    "LIFT KEY" : {
+        "itemLocation": "Rocket Hideout 4",
+        "imgLink": "./Images/keyItems/liftkey.webp",
+        "hasCheckedLocation": false
+    },
+    "OAK'S PARCEL" : {
+        "itemLocation": "Viridian City - Mart",
+        "imgLink": "./Images/keyItems/oak'sparcel.webp",
+        "hasCheckedLocation": false
+    },
+    "OLD ROD" : {
+        "itemLocation": "Vermilion City - House 2",
+        "imgLink": "./Images/keyItems/oldrod.webp",
+        "hasCheckedLocation": false
+    },
+    "HM05: Flash" : {
+        "itemLocation": "Route 2 - Gate",
+        "imgLink": "./Images/keyItems/HM05.webp",
+        "hasCheckedLocation": false
+    },
+    "HM01: Cut" : {
+        "itemLocation": "SS Anne - Captain's Room",
+        "imgLink": "./Images/keyItems/HM01.webp",
+        "hasCheckedLocation": false
+    },
+    "HM02: Fly" : {
+        "itemLocation": "Route 16 - House",
+        "imgLink": "./Images/keyItems/HM02.webp",
+        "hasCheckedLocation": false
+    },
+    "OLD AMBER" : {
+        "itemLocation": "Pewter City - Museum 1F",
+        "imgLink": "./Images/keyItems/oldamber.webp",
+        "hasCheckedLocation": false
+    },
+    "SILPH SCOPE" : {
+        "itemLocation": "Rocket Hideout 4",
+        "imgLink": "./Images/keyItems/silphscope.webp",
+        "hasCheckedLocation": false
+    },
+    "POKé FLUTE" : {
+        "itemLocation": "Lavender Town - House 1",
+        "imgLink": "./Images/keyItems/pokeflute.webp",
+        "hasCheckedLocation": false
+    },
+    "SUPER ROD" : {
+        "itemLocation": "Route 12 - House",
+        "imgLink": "./Images/keyItems/superrod.webp",
+        "hasCheckedLocation": false
+    },
+    "HM03: Surf" : {
+        "itemLocation": "Safari Zone - Secret House",
+        "imgLink": "./Images/keyItems/HM03.webp",
+        "hasCheckedLocation": false
+    },
+    "HM04: Strength" : {
+        "itemLocation": "Fuchsia City - House 3",
+        "imgLink": "./Images/keyItems/HM04.webp",
+        "hasCheckedLocation": false
+    },
+    "SECRET KEY" : {
+        "itemLocation": "Cinnabar Mansion",
+        "imgLink": "./Images/keyItems/secretkey.webp",
+        "hasCheckedLocation": false
+    },
+    "S.S.TICKET" : {
+        "itemLocation": "Bill's House",
+        "imgLink": "./Images/keyItems/ssticket.webp",
+        "hasCheckedLocation": false
+    }
+}];
